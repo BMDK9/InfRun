deef5158c93760b77ee0bde90ca66f85cbf00fe5
BMDK9 <qnrakdlzm77@naver.com>
2024-03-27 21:18:15 +0900

    feat : 직접 빈으로 등록

--- src/main/java/infrun/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/infrun/hellospring/repository/MemoryMemberRepository.java
@@ -14,7 +14,7 @@ public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-//    @Override
+    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
