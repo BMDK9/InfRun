7a9335e631a855b05f29152d2b13a2512d6c2a2b
BMDK9 <qnrakdlzm77@naver.com>
2024-03-28 21:27:30 +0900

    feat : 멤버 컨트롤러

--- src/main/java/infrun/hellospring/controller/MemberController.java
+++ src/main/java/infrun/hellospring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package infrun.hellospring.controller;
 
+import infrun.hellospring.domain.Member;
 import infrun.hellospring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +17,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.signIn(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String getAllMembers(Model model) {
+        List<Member> allMembers = memberService.findAllMembers();
+        model.addAttribute("members", allMembers);
+
+        return "/members/memberList";
     }
 }
