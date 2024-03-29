582929c73983c7a4af1328c87017759918fe714e
BMDK9 <qnrakdlzm77@naver.com>
2024-03-29 20:14:13 +0900

    JPA

--- src/main/java/infrun/hellospring/SpringConfig.java
+++ src/main/java/infrun/hellospring/SpringConfig.java
@@ -1,9 +1,9 @@
 package infrun.hellospring;
 
-import infrun.hellospring.repository.JdbcTemplateMemberRepository;
+import infrun.hellospring.repository.JpaMemberRepository;
 import infrun.hellospring.repository.MemberRepository;
-import infrun.hellospring.repository.MemoryMemberRepository;
 import infrun.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    //    private DataSource dataSource;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -27,6 +32,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/infrun/hellospring/service/MemberService.java
+++ src/main/java/infrun/hellospring/service/MemberService.java
@@ -7,8 +7,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //@Service
+@Transactional
 public class MemberService {
 
 
